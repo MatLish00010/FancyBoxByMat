1aa468e5af8cac10e618b2db27658788627e1b84
Matvey <lishay.matvey20001506@gmail.com>
2020-01-22 23:59:06 +0300

    delite one span (stop)

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -70,35 +70,15 @@
     var time = null;
 
     $startPlay.click(function () {
-        time =
-            setInterval(function () {
-                $next.click();
-            }, 500);
+        if (time == null) {
+            time =
+                setInterval(function () {
+                    $next.click();
+                }, 500);
+        } else {
+            clearInterval(time);
+            time = null;
+        }
     });
-
-    $stopPlay.click(function () {
-        clearInterval(time);
-    });
-
-
 })(jQuery);
 
-/*
-var pictures = [
-    'images/big_img/Cats_1.jpg',
-    'images/big_img/Cats_2.jpg',
-    'images/big_img/Cats_3.jpg'
-];
-
-var thumbnails = document.querySelectorAll('.gallery__picture-preview');
-var fullPhoto = document.querySelector('.full-picture');
-
-var addThumbnailClickHandler = function (thumbnail, photo) {
-    thumbnail.addEventListener('click', function () {
-        fullPhoto.src = photo;
-    })
-};
-
-for (var i = 0; i < thumbnails.length; i++){
-    addThumbnailClickHandler(thumbnails[i], pictures[i]);
-}*/
